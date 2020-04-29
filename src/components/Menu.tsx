fe87833a0f00fc45c80f673989110648a8d66f8f
Houjun Liu <hliu@shabang.cf>
2020-04-29 10:29:14 -0700

    Working routing (in the menu!)

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -10,8 +10,8 @@ import {
   IonNote,
 } from '@ionic/react';
 
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, {Component} from 'react';
+import { RouteComponentProps, withRouter } from 'react-router';
 import { chevronForwardCircle } from 'ionicons/icons';
 import './Menu.css';
 import '../backend/CacheManager';
@@ -24,33 +24,37 @@ let ps : String[] = [];
     ps.push(e[2].map((p:any)=>p.name));
 }));
 
-const Menu: React.FC = () => {
-  const location = useLocation();
+class Menu extends Component<{} & RouteComponentProps<{}>, {}>{
+    constructor(props:any) {
+        super(props);
+    }
 
-  return (
-    <IonMenu contentId="main" type="overlay">
-      <IonContent>
-        <IonList>
-              <IonMenuToggle key={0} autoHide={false}>
-                  <IonItem className={location.pathname === "/page/Page" ? 'selected' : ''} routerLink={"/page/Page"} routerDirection="none" lines="none" detail={false}>
-                      <IonIcon slot="start" icon={chevronForwardCircle} color="#000000"/>
-                      <IonLabel id="menu-upcoming">Upcoming</IonLabel>
-                    </IonItem>
-              </IonMenuToggle>
-              <IonLabel class="menu-label">Perspectives</IonLabel>
-            {ps.map((pName:String)=>{
-                return (
-                    <IonItem className={location.pathname === "/page/Perspective" ? 'selected' : ''} routerLink={"/page/Perspsective"} routerDirection="none" lines="none" detail={false}>
-                        <IonIcon slot="start" icon={chevronForwardCircle} color="#000000"/>
-                        <IonLabel id="menu-upcoming">{pName}</IonLabel>
-                    </IonItem>
+    render() {
+        const { location } = this.props;
+        return (<IonMenu contentId="main" type="overlay">
+          <IonContent>
+            <IonList>
+                  <IonMenuToggle key={0} autoHide={false}>
+                      <IonItem className={location.pathname === "/" ? 'selected' : ''} routerLink={"/page/Page"} routerDirection="none" lines="none" detail={false}>
+                          <IonIcon slot="start" icon={chevronForwardCircle} color="#000000"/>
+                          <IonLabel id="menu-upcoming">Upcoming</IonLabel>
+                        </IonItem>
+                  </IonMenuToggle>
+                  <IonLabel class="menu-label">Perspectives</IonLabel>
+                  {ps.map((pName:String)=>{
+                    return (
+                        <IonItem className={location.pathname === "/page/Perspective" ? 'selected' : ''} routerLink={"/page/Perspsective"} routerDirection="none" lines="none" detail={false}>
+                            <IonIcon slot="start" icon={chevronForwardCircle} color="#000000"/>
+                            <IonLabel id="menu-upcoming">{pName}</IonLabel>
+                        </IonItem>
 
-                );
-            })} 
-        </IonList>
-      </IonContent>
-    </IonMenu>
-  );
-};
+                    );
+                  })} 
+            </IonList>
+          </IonContent>
+        </IonMenu>);
+    };
+}
 
-export default Menu;
+export default withRouter(Menu);
+
