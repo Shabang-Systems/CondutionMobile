e5dde5737492d08743196468429b10c773784b9a
Houjun Liu <hliu@shabang.cf>
2020-04-30 21:35:07 -0700

    Added backend submodules

---
+++ src/pages/Upcoming.tsx
@@ -0,0 +1,27 @@
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonText, IonIcon, IonLabel, IonList, IonItem } from '@ionic/react';
+import { chevronForwardCircle, layers, albums } from 'ionicons/icons';
+import React, {Component} from 'react';
+import { useParams } from 'react-router';
+import './Upcoming.css';
+
+
+class Upcoming extends Component<{},{}>{
+    render() {
+        return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar class="head-toolbar">
+                    <IonButtons slot="start"> 
+                        <IonMenuButton />
+                    </IonButtons>
+                    <IonTitle slot="start">Upcoming</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+            </IonContent>
+        </IonPage>
+        );
+    }
+};
+
+export default Upcoming;
